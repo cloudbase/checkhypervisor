932f390d62177a143fe57d007a62d798d4bfa64c
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2014-08-03 23:41:08 -0400

    Fixes issue with cpuid call

--- checkhypervisor.c
+++ checkhypervisor.c
@@ -14,30 +14,52 @@
 *    under the License.
 */
 
-#include <cpuid.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+void get_cpuid(unsigned cpuid, unsigned* eax, unsigned* ebx, unsigned* ecx, unsigned* edx) 
+{
+    unsigned a = *eax;
+    unsigned b = *ebx;
+    unsigned c = *ecx;
+    unsigned d = *edx;
+
+    __asm__ __volatile__ ("cpuid": "=a" (a), "=b" (b), "=c" (c), "=d" (d) : "a" (cpuid));
+
+    *eax = a;
+    *ebx = b;
+    *ecx = c;
+    *edx = d;
+}
+
+/*
+Hyper-V specs: http://www.microsoft.com/en-us/download/confirmation.aspx?id=39289
+VMWare specs: http://kb.vmware.com/selfservice/microsites/search.do?language=en_US&cmd=displayKC&externalId=1009458
+*/
 int get_hypervisor_id(char* id)
 {
-    unsigned int eax = 0;
-    unsigned int ebx = 0;
-    unsigned int ecx = 0;
-    unsigned int edx = 0;
+    unsigned eax = 0;
+    unsigned ebx = 0;
+    unsigned ecx = 0;
+    unsigned edx = 0;
     
     id[0] = 0;
 
-    if(__get_cpuid(1, &eax, &ebx, &ecx, &edx) && (ecx & (1 << 31)))
+    get_cpuid(1, &eax, &ebx, &ecx, &edx);
+    if(ecx & (1 << 31))
     {
-        if(__get_cpuid(0x40000000, &eax, &ebx, &ecx, &edx))
-        {
-            *((unsigned int*)id) = ebx;
-            *((unsigned int*)id + 1) = ecx;
-            *((unsigned int*)id + 2) = edx;
-            id[12] = 0;
+        eax = 0;
+        ebx = 0;
+        ecx = 0;
+        edx = 0;
 
-            return 1;
-        }
+        get_cpuid(0x40000000, &eax, &ebx, &ecx, &edx);
+        *((unsigned*)id) = ebx;
+        *((unsigned*)id + 1) = ecx;
+        *((unsigned*)id + 2) = edx;
+        id[12] = 0;
+
+        return 1;
     }
 
     return 0;
